3acd0b4c1718a554006c07c7cd060a9c84fdd84d
Mahesh Chand <mahesh.yadav.107@gmail.com>
2014-05-08 16:08:57 +0530

    Updating performacen test to validate against a benchmark number

--- test/performance/MongoTripodConfigTest.php
+++ test/performance/MongoTripodConfigTest.php
@@ -15,10 +15,14 @@ require_once 'src/mongo/MongoTripodConfig.class.php';
 class MongoTripodConfigTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Used in capturing start time of test in micro-seconds
-     * @var String
+     * time in ms (milli-seconds) anything below which is acceptable.
      */
-    private $test_start_time = null;
+    const BENCHMARK_OBJECT_CREATE_TIME = 4000;
+
+    /**
+     * Number of iterations should to be ran to test
+     */
+    const BENCHMARK_OBJECT_CREATE_ITERATIONS = 1000;
 
     /**
      * Holds tripod config
@@ -33,36 +37,53 @@ class MongoTripodConfigTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setup();
-
         $this->config = json_decode(file_get_contents(dirname(__FILE__) . '/tripodConfig.json'), true);
-        $this->test_start_time = microtime();
     }
 
     /**
      * Post test completion actions.
-     * e.g. Calculate time taken by test.
      */
     protected function tearDown()
     {
         $this->config = array();
-        $test_end_time  = microtime();
-
-        list($endTimeMicroSeconds, $endTimeSeconds) = explode(' ', $test_end_time);
-        list($startTimeMicroSeconds, $startTimeSeconds) = explode(' ', $this->test_start_time);
-
-        $differenceInMilliSeconds =  ((float)$endTimeSeconds - (float)$startTimeSeconds)*1000;
-
-        echo "\n". $this->getName(FALSE) . " : Total time taken (ms) : " . round(($differenceInMilliSeconds + ((float)$endTimeMicroSeconds *1000)) -  (float)$startTimeMicroSeconds *1000);
+        parent::tearDown();
     }
 
     /**
+     * Note: Current version of this test tried to create 1000 objects within 4000ms which is reasonable at this time.
+     *       Any change to this class if make it a more a big number it should be validated and tested to ensure performance impact.
+     *
      * Create some instances of TripodConfig to see what amount of time is taken in creating instance and processing in constructor.
      */
     public function testCreateMongoTripodConfigObject()
     {
-        //Let's try to create 50 objects to see how much time they take.
-        for($i =0; $i<50; $i++) {
+        $testStartTime = microtime();
+
+        //Let's try to create 1000 objects to see how much time they take.
+        for($i =0; $i < self::BENCHMARK_OBJECT_CREATE_ITERATIONS; $i++) {
             $instance = new MongoTripodConfig($this->config);
         }
+
+        $testEndTime = microtime();
+        $this->$this->assertLessThan(
+            self::BENCHMARK_TIME_OBJECT_CREATE,
+            $this->getTimeDifference($testStartTime, $testEndTime),
+            "It should always take less than " . self::BENCHMARK_TIME_OBJECT_CREATE . "ms to create " . self::BENCHMARK_OBJECT_CREATE_ITERATIONS . " objects of MongoTripodConfig class"
+        );
+    }
+
+    /**
+     * Helper function to calculate difference between two microtime values
+     * @param $microTime1, time string from microtime()
+     * @param $microTime2, time string from microtime()
+     * @return float, time difference in ms
+     */
+    private function getTimeDifference($microTime1, $microTime2){
+        list($endTimeMicroSeconds, $endTimeSeconds) = explode(' ', $microTime2);
+        list($startTimeMicroSeconds, $startTimeSeconds) = explode(' ', $microTime1);
+
+        $differenceInMilliSeconds =  ((float)$endTimeSeconds - (float)$startTimeSeconds)*1000;
+
+        return round(($differenceInMilliSeconds + ((float)$endTimeMicroSeconds *1000)) -  (float)$startTimeMicroSeconds *1000);
     }
 }
