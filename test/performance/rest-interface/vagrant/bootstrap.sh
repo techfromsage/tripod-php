e3e0d79b983fd1c8f6b2275e4199cbbcc6bc2e74
MalcyL <malcolm@landonsonline.me.uk>
2015-02-10 11:38:31 +0000

    rest-interface bootstrap changes.

--- test/performance/rest-interface/vagrant/bootstrap.sh
+++ test/performance/rest-interface/vagrant/bootstrap.sh
@@ -4,15 +4,8 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 apt-get update -y
 apt-get install -y mongodb-org -y
 apt-get install php5 php-pear -y
-pecl install mongo
+yes '' | pecl install mongo
 apt-get install git siege -y
-git clone https://github.com/talis/tripod-php.git /opt/tripod-php
-cd /opt/tripod-php
-git checkout performance-test-rest-interface
-ln -s /opt/tripod-php/test/performance/rest-interface /var/www/
-cd /opt/tripod-php/test/performance/rest-interface/
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
 a2enmod rewrite
 touch /etc/php5/apache2/conf.d/mongo.ini
 echo "extension=mongo.so" > /etc/php5/apache2/conf.d/mongo.ini
