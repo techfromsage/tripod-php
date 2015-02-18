15258dbc6144899105ace613fc887415cda7d3b9
Ross Singer <rossfsinger@gmail.com>
2015-02-18 06:43:34 +0000

    Use mongodb-10gen, not mongodb-org

--- test/performance/rest-interface/vagrant/bootstrap.sh
+++ test/performance/rest-interface/vagrant/bootstrap.sh
@@ -2,7 +2,7 @@ apt-get update -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get update -y
-apt-get install -y mongodb-org -y
+apt-get install -y mongodb-10gen=2.2.2 -y
 apt-get install php5 php-pear -y
 yes '' | pecl install mongo
 apt-get install git siege -y
