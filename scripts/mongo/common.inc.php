510f60ffde9b8bb7d0e66eab9673c27ac1c55e80
Ross Singer <rossfsinger@gmail.com>
2015-06-08 08:50:35 +0100

    Correct path

--- scripts/mongo/common.inc.php
+++ scripts/mongo/common.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../../vendor/autoload.php') as $file) {
+foreach (array(__DIR__ . '/../../../../autoload.php', __DIR__ . '/../../vendor/autoload.php') as $file) {
     if (file_exists($file)) {
         define('TRIPOD_COMPOSER_INSTALL', $file);
         break;
