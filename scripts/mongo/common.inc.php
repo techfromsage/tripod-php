f63ce9bc4b0a831380f0011cfdc0bc85126caffe
Ross Singer <rossfsinger@gmail.com>
2015-06-22 11:49:53 +0100

    Document script autoloader thing

--- scripts/mongo/common.inc.php
+++ scripts/mongo/common.inc.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * This will load the composer autoloader depending on whether or not Tripod is the project (i.e. Tripod contains
+ * a vendor subdirectory) or if Tripod is a dependency of another project (i.e. Tripod is *in* a vendor subdirectory)
+ */
 foreach (array(__DIR__ . '/../../../../autoload.php', __DIR__ . '/../../vendor/autoload.php') as $file) {
     if (file_exists($file)) {
         define('TRIPOD_COMPOSER_INSTALL', $file);
@@ -10,7 +13,7 @@ foreach (array(__DIR__ . '/../../../../autoload.php', __DIR__ . '/../../vendor/a
 if (!defined('TRIPOD_COMPOSER_INSTALL')) {
     die(
         'You need to set up the project dependencies using the following commands:' . PHP_EOL .
-        'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+        'curl -sS https://getcomposer.org/installer | php' . PHP_EOL .
         'php composer.phar install' . PHP_EOL
     );
 }
