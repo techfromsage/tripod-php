9b4993d632a878bd8483dd3495a5fade95eb0719
Chris Clarke <chris.clarke@talis.com>
2015-07-09 10:32:27 +0100

    Fixes #72
    Fixes #72
    
    Fixing code review comments

---
+++ src/exceptions/JobException.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tripod\Exceptions;
+
+// @codeCoverageIgnoreStart
+
+/**
+ * Class SearchException
+ * @package Tripod
+ */
+class JobException extends Exception {
+
+} // @codeCoverageIgnoreEnd
