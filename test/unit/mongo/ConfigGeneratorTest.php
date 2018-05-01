206d3641addebf42dba36c12898b5925de9df7fa
Ross Singer <rossfsinger@gmail.com>
2018-05-01 16:28:45 -0400

    Test to ensure discover jobs get serialized config

--- test/unit/mongo/ConfigGeneratorTest.php
+++ test/unit/mongo/ConfigGeneratorTest.php
@@ -5,14 +5,18 @@ require_once 'TestConfigGenerator.php';
 
 class ConfigGeneratorTest extends MongoTripodTestBase
 {
+    private $config = [];
+
+    public function setUp()
+    {
+        $this->config = [
+            'class' => 'TestConfigGenerator',
+            'filename' => dirname(__FILE__) . '/data/config.json'
+        ];
+        \Tripod\Config::setConfig($this->config);
+    }
     public function testCreateFromConfig()
     {
-        \Tripod\Config::setConfig(
-            [
-                'class' => 'TestConfigGenerator',
-                'filename' => dirname(__FILE__) . '/data/config.json'
-            ]
-        );
         /** @var TestConfigGenerator $instance */
         $instance = \Tripod\Config::getInstance();
         $this->assertInstanceOf('TestConfigGenerator', $instance);
@@ -26,14 +30,70 @@ class ConfigGeneratorTest extends MongoTripodTestBase
 
     public function testSerializeConfig()
     {
-        $config = [
-            'class' => 'TestConfigGenerator',
-            'filename' => dirname(__FILE__) . '/data/config.json'
-        ];
-        \Tripod\Config::setConfig($config);
-
         /** @var TestConfigGenerator $instance */
         $instance = \Tripod\Config::getInstance();
-        $this->assertEquals($config, $instance->serialize());
+        $this->assertEquals($this->config, $instance->serialize());
+    }
+
+    public function testConfigGeneratorsSerializedInDiscoverJobs()
+    {
+        $originalGraph = new \Tripod\ExtendedGraph();
+        $originalGraph->add_resource_triple('http://example.com/1', RDF_TYPE, RDFS_CLASS);
+
+        $newGraph = new \Tripod\ExtendedGraph();
+        $newGraph->add_resource_triple('http://example.com/1', RDF_TYPE, OWL_CLASS);
+        $subjectsAndPredicatesOfChange = ['http://example.com/1' => [RDF_TYPE]];
+
+        /** @var \Tripod\Mongo\Driver|PHPUnit_Framework_MockObject_MockObject $tripod */
+        $tripod = $this->getMockBuilder('\Tripod\Mongo\Driver')
+            ->setMethods(['getDataUpdater'])
+            ->setConstructorArgs(
+                ['CBD_testing', 'tripod_php_testing']
+            )
+            ->getMock();
+
+        /** @var \Tripod\Mongo\Updates|PHPUnit_Framework_MockObject_MockObject $updates */
+        $updates = $this->getMockBuilder('\Tripod\Mongo\Updates')
+            ->setMethods(
+                [
+                    'applyHooks',
+                    'storeChanges',
+                    'setReadPreferenceToPrimary',
+                    'processSyncOperations',
+                    'getDiscoverImpactedSubjects',
+                    'resetOriginalReadPreference'
+                ]
+            )
+            ->setConstructorArgs([$tripod])
+            ->getMock();
+
+        /** @var \Tripod\Mongo\Jobs\DiscoverImpactedSubjects|PHPUnit_Framework_MockObject_MockObject $discoverJob */
+        $discoverJob = $this->getMockBuilder('\Tripod\Mongo\Jobs\DiscoverImpactedSubjects')
+            ->setMethods(['createJob'])
+            ->getMock();
+
+        $tripod->expects($this->once())->method('getDataUpdater')->will($this->returnValue($updates));
+        $updates->expects($this->once())->method('getDiscoverImpactedSubjects')->will($this->returnValue($discoverJob));
+
+        $updates->expects($this->once())->method('storeChanges')->will(
+            $this->returnValue(
+                ['transaction_id' => uniqid(), 'subjectsAndPredicatesOfChange' => $subjectsAndPredicatesOfChange]
+            )
+        );
+
+        $discoverJob->expects($this->once())->method('createJob')
+            ->with([
+                'changes' => $subjectsAndPredicatesOfChange,
+                'operations' => [OP_TABLES, OP_SEARCH],
+                'storeName' => 'tripod_php_testing',
+                'podName' => 'CBD_testing',
+                'contextAlias' => 'http://talisaspire.com/',
+                'statsConfig' => []
+            ]);
+
+        $tripod->saveChanges(
+            $originalGraph,
+            $newGraph
+        );
     }
 }
