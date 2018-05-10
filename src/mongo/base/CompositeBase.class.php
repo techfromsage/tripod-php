a969a31afea6fce7e8c21a839b65cd15ca5d3225
Ross Singer <rossfsinger@gmail.com>
2018-05-10 12:26:18 -0400

    Inadvertently left out of last commit

--- src/mongo/base/CompositeBase.class.php
+++ src/mongo/base/CompositeBase.class.php
@@ -31,6 +31,12 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
             $filter[] = [_ID_RESOURCE => $resourceAlias, _ID_CONTEXT => $contextAlias];
         }
         $query = [_ID_KEY => ['$in' => $filter]];
+        if ($timestamp) {
+            if (!$timestamp instanceof \MongoDB\BSON\UTCDateTime) {
+                $timestamp = $this->getMongoDate($timestamp);
+            }
+            $query[_CREATED_TS] = ['$lte' => $timestamp];
+        }
         $docs = $this->getCollection()->find(
             $query,
             ['projection' => [_ID_KEY=>true, 'rdf:type'=>true]]
@@ -45,11 +51,11 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
                 $docHash     = md5($docResource.$docContext);
 
                 $docTypes = array();
-                if (isset($doc["rdf:type"])) {
-                    if (isset($doc["rdf:type"][VALUE_URI])) {
-                        $docTypes[] = $doc["rdf:type"][VALUE_URI];
+                if (isset($doc['rdf:type'])) {
+                    if (isset($doc['rdf:type'][VALUE_URI])) {
+                        $docTypes[] = $doc['rdf:type'][VALUE_URI];
                     } else {
-                        foreach ($doc["rdf:type"] as $t) {
+                        foreach ($doc['rdf:type'] as $t) {
                             if (isset($t[VALUE_URI])) {
                                 $docTypes[] = $t[VALUE_URI];
                             }
@@ -57,25 +63,20 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
                     }
                 }
 
-                $currentSubjectProperties = array();
-                if(isset($subjectsAndPredicatesOfChange[$docResource]))
-                {
+                $currentSubjectProperties = [];
+                if (isset($subjectsAndPredicatesOfChange[$docResource])) {
                     $currentSubjectProperties = $subjectsAndPredicatesOfChange[$docResource];
-                }
-                elseif(isset($subjectsToAlias[$docResource]) &&
-                    isset($subjectsAndPredicatesOfChange[$subjectsToAlias[$docResource]]))
-                {
+                } elseif (isset($subjectsToAlias[$docResource]) &&
+                    isset($subjectsAndPredicatesOfChange[$subjectsToAlias[$docResource]])) {
                     $currentSubjectProperties = $subjectsAndPredicatesOfChange[$subjectsToAlias[$docResource]];
                 }
-                foreach($docTypes as $type)
-                {
-                    if($this->checkIfTypeShouldTriggerOperation($type, $types, $currentSubjectProperties)) {
-                        if(!array_key_exists($this->getPodName(), $candidates))
-                        {
-                            $candidates[$this->getPodName()] = array();
+                foreach ($docTypes as $type) {
+                    if ($this->checkIfTypeShouldTriggerOperation($type, $types, $currentSubjectProperties)) {
+                        if (!array_key_exists($this->getPodName(), $candidates)) {
+                            $candidates[$this->getPodName()] = [];
                         }
-                        if(!array_key_exists($docHash, $candidates[$this->getPodName()])){
-                            $candidates[$this->getPodName()][$docHash] = array('id'=>$doc[_ID_KEY]);
+                        if (!array_key_exists($docHash, $candidates[$this->getPodName()])) {
+                            $candidates[$this->getPodName()][$docHash] = ['id'=>$doc[_ID_KEY]];
                         }
                     }
                 }
@@ -83,41 +84,44 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
         }
 
         // add to this any composites
-        foreach($this->findImpactedComposites($subjectsAndPredicatesOfChange, $contextAlias) as $doc) {
+        foreach ($this->findImpactedComposites($subjectsAndPredicatesOfChange, $contextAlias) as $doc) {
             $spec = $this->getSpecification($this->storeName, $doc[_ID_KEY]['type']);
-            if(is_array($spec) && array_key_exists('from', $spec)){
-                if(!array_key_exists($spec['from'], $candidates))
-                {
+            if (is_array($spec) && array_key_exists('from', $spec)) {
+                if (!array_key_exists($spec['from'], $candidates)) {
                     $candidates[$spec['from']] = array();
                 }
                 $docHash = md5($doc[_ID_KEY][_ID_RESOURCE] . $doc[_ID_KEY][_ID_CONTEXT]);
 
-                if(!array_key_exists($docHash, $candidates[$spec['from']])){
-                    $candidates[$spec['from']][$docHash] = array(
-                        'id'=>array(
-                            _ID_RESOURCE=>$doc[_ID_KEY][_ID_RESOURCE],
-                            _ID_CONTEXT=>$doc[_ID_KEY][_ID_CONTEXT],
-                        )
-                    );
+                if (!array_key_exists($docHash, $candidates[$spec['from']])) {
+                    $candidates[$spec['from']][$docHash] = [
+                        'id'=> [
+                            _ID_RESOURCE => $doc[_ID_KEY][_ID_RESOURCE],
+                            _ID_CONTEXT => $doc[_ID_KEY][_ID_CONTEXT],
+                        ]
+                    ];
                 }
-                if(!array_key_exists('specTypes', $candidates[$spec['from']][$docHash])) {
-                    $candidates[$spec['from']][$docHash]['specTypes'] = array();
+                if (!array_key_exists('specTypes', $candidates[$spec['from']][$docHash])) {
+                    $candidates[$spec['from']][$docHash]['specTypes'] = [];
                 }
                 // Save the specification type so we only have to regen resources in that table type
-                if(!in_array($doc[_ID_KEY][_ID_TYPE], $candidates[$spec['from']][$docHash]['specTypes']))
-                {
+                if (!in_array($doc[_ID_KEY][_ID_TYPE], $candidates[$spec['from']][$docHash]['specTypes'])) {
                     $candidates[$spec['from']][$docHash]['specTypes'][] = $doc[_ID_KEY][_ID_TYPE];
                 }
             }
         }
 
         // convert operations to subjects
-        $impactedSubjects = array();
-        foreach(array_keys($candidates) as $podName){
-            foreach($candidates[$podName] as $candidate)
-            {
-                $specTypes = (isset($candidate['specTypes']) ? $candidate['specTypes'] : array());
-                $impactedSubjects[] = new \Tripod\Mongo\ImpactedSubject($candidate['id'], $this->getOperationType(), $this->getStoreName(), $podName, $specTypes);
+        $impactedSubjects = [];
+        foreach (array_keys($candidates) as $podName) {
+            foreach ($candidates[$podName] as $candidate) {
+                $specTypes = isset($candidate['specTypes']) ? $candidate['specTypes'] : [];
+                $impactedSubjects[] = new \Tripod\Mongo\ImpactedSubject(
+                    $candidate['id'],
+                    $this->getOperationType(),
+                    $this->getStoreName(),
+                    $podName,
+                    $specTypes
+                );
             }
         }
 
@@ -128,14 +132,14 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
      * Returns an array of the rdf types that will trigger the specification
      * @return array
      */
-    public abstract function getTypesInSpecifications();
+    abstract public function getTypesInSpecifications();
 
     /**
      * @param array $resourcesAndPredicates
      * @param string $contextAlias
      * @return mixed // @todo: This may eventually return a either a Cursor or array
      */
-    public abstract function findImpactedComposites(Array $resourcesAndPredicates,$contextAlias);
+    abstract public function findImpactedComposites(array $resourcesAndPredicates, $contextAlias);
 
     /**
      * Returns the specification config
@@ -143,7 +147,7 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
      * @param string $specId The specification id
      * @return array|null
      */
-    public abstract function getSpecification($storeName, $specId);
+    abstract public function getSpecification($storeName, $specId);
 
     /**
      * Test if the a particular type appears in the array of types associated with a particular spec and that the changeset
@@ -153,20 +157,20 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
      * @param array $subjectPredicates
      * @return bool
      */
-    protected function checkIfTypeShouldTriggerOperation($rdfType, array $validTypes, Array $subjectPredicates)
+    protected function checkIfTypeShouldTriggerOperation($rdfType, array $validTypes, array $subjectPredicates)
     {
         // We don't know if this is an alias or a fqURI, nor what is in the valid types, necessarily
         $types = array($rdfType);
-        try
-        {
+        try {
             $types[] = $this->labeller->qname_to_uri($rdfType);
+        } catch (\Tripod\Exceptions\LabellerException $e) {
+            // Ignore
         }
-        catch(\Tripod\Exceptions\LabellerException $e) {}
-        try
-        {
+        try {
             $types[] = $this->labeller->uri_to_alias($rdfType);
+        } catch (\Tripod\Exceptions\LabellerException $e) {
+            // Ignore
         }
-        catch(\Tripod\Exceptions\LabellerException $e) {}
 
         $intersectingTypes = array_unique(array_intersect($types, $validTypes));
         // If views have a matching type *at all*, the operation is triggered
@@ -180,8 +184,7 @@ abstract class CompositeBase extends \Tripod\Mongo\DriverBase implements \Tripod
      */
     protected function getApplyOperation()
     {
-        if(!isset($this->applyOperation))
-        {
+        if (!isset($this->applyOperation)) {
             $this->applyOperation = new \Tripod\Mongo\Jobs\ApplyOperation();
         }
         return $this->applyOperation;
