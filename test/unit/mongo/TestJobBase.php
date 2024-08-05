b3598b2d0d625f80d149456aebdebdce00c12d35
Grzegorz Rajchman <grzegorz.rajchman@technologyfromsage.com>
2024-08-05 12:12:59 +0100

    Add test to cover JobBase::getTripodConfig

---
+++ test/unit/mongo/TestJobBase.php
@@ -0,0 +1,27 @@
+<?php
+
+class TestJobBase extends \Tripod\Mongo\Jobs\JobBase
+{
+    /**
+     * Expose this method for testing
+     * @inheritDoc
+     */
+    public function getTripodConfig()
+    {
+        return parent::getTripodConfig();
+    }
+
+    public function perform()
+    {
+    }
+
+    protected function getStatTimerSuccessKey()
+    {
+        return 'TEST_SUCCESS';
+    }
+
+    protected function getStatFailureIncrementKey()
+    {
+        return 'TEST_FAIL';
+    }
+}
